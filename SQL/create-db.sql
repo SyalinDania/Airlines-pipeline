50c4a76cbed952a066918089eb6d22035fe681c0
SyalinDania <37008778+SyalinDania@users.noreply.github.com>
2021-07-24 00:37:44 +0800

    Add files via upload

---
+++ SQL/create-db.sql
@@ -0,0 +1,15 @@
+CREATE TABLE airports (
+ID int,
+Name varchar(100),
+City varchar(100),
+Country varchar(100),
+IATA varchar(10),
+ICAO varchar(10),
+Latitude decimal(10,8),
+Longitude decimal(11,8),
+Altitude int,
+Offset decimal,
+DST varchar(10),
+Timezone varchar (100),
+
+);
